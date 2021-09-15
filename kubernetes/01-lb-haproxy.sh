561d56e046bacc1d6da2a668bd99ad005025c9ef
Andika Ahmad Ramadhan <andikahmadr@gmail.com>
2021-09-15 07:22:49 +0700

    configure docker for kubernetes

--- kubernetes/01-lb-haproxy.sh
+++ kubernetes/01-lb-haproxy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
+    echo "This script must be run as root" 1>&2
+    exit 1
 fi
 
 printf "Open firewall\n"
--- kubernetes/06-master-bootstrap.sh
+++ kubernetes/06-master-bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubeadm init --control-plane-endpoint "192.168.0.110:6443" --upload-certs --pod-network-cidr=192.168.0.0/16
+kubeadm init --control-plane-endpoint "HAPROXY_HOST:6443" --upload-certs --pod-network-cidr=10.10.0.0/16
 
 mkdir -p $HOME/.kube
   sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
